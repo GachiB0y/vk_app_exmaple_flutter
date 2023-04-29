60a32936f771f703ab3959275a8bac34d0d2cd28
GachiB0y <91989567+GachiB0y@users.noreply.github.com>
2023-04-29 12:09:25 +0300

    Add files via upload
    upload file

--- lib/configuration/configuration.dart
+++ lib/configuration/configuration.dart
@@ -1,6 +1,6 @@
 class Configuration{
   static const host = 'https://api.vk.com';
   static const getMethodUrl = "https://api.vk.com/method";
-  static const tokenUser = 'userToken';
+  static const tokenUser = 'TokenUser';
   static const userId ='161131685';
 }
